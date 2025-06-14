c78c14adf761f8c5daf799cb23602a0d81053bbb
juanhefis13 <juanfranciscoleal13@gmail.com>
2025-06-13 22:34:24 -0400

    Actualización de la página web

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'src')));
+// Servir archivos estáticos desde la carpeta raíz 'Websites-proyect'
+app.use(express.static(path.join(__dirname)));
 
+// Ruta raíz (/) sirve el index.html de la carpeta Inicio
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+    res.sendFile(path.join(__dirname, 'Inicio', 'index.html'));
+});
+
+// Manejar rutas no encontradas
+app.use((req, res) => {
+    res.status(404).send('Página no encontrada');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
